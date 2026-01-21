0bc22a9259de4eeca84dd28b5c3d1fe5d8b1b5ea
Prasan <prasan@internshala.com>
2026-01-21 11:20:03 +0530

    Build : Added atomicity and predictable file writes

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -21,8 +21,8 @@ import (
 
 // The data is probably still in the operating system’s page cache after the write syscall returns so the file state might be broken if te system
 // crashes and reboots
-func SaveData(path string, data []byte) error{
-	fd, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC,0664)
+func SaveData(path string, data []byte) error {
+	fd, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 
 	if err != nil {
 		log.Fatal(err)
@@ -35,35 +35,90 @@ func SaveData(path string, data []byte) error{
 	return err
 }
 
-// to solve some of these problems lets make the file writing processs atomic
+// to solve some of these problems lets make the file writing process atomic
 
-//we will create a tmp file first and write our data in that then 
+//we will create a tmp file first and write our data in that then
 // when that is done we will rename our tmp file with the orignal file name
-// after deleting the orignal file , Renaming a file in os is atomic(its either done or not done there is nothing in between these two states)
+// after deleting the orignal file , Renaming a file in unix based os is atomic ( ie its either done or not done there is nothing in between these two states)
 
-func SaveData2(path string, data []byte) error{
+//In this new implementation we fixed one thing ie Made our data updation atomic now
+// when we update our file we have either of the two states File updated or FIle not updated
+//There is nothing in between
 
-	rand.Seed(time.Now().UnixNano())
+func SaveData2(path string, data []byte) error {
 
-    // Generate a random integer between 0 and 99
-    x := rand.Intn(100)
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+	randomFileName := r.Intn(100)
 
-	tmpPath := fmt.Sprintf("%s.tmp.%d", path, x)
+	tmpPath := fmt.Sprintf("%s.tmp.%d", path, randomFileName)
 
 	fp, err := os.OpenFile(tmpPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer fp.Close()
+
+	_, err = fp.Write(data)
+
+	if err != nil {
+		os.Remove(tmpPath)
+		log.Fatal(err)
+	}
+
+	return os.Rename(tmpPath, path)
+}
+
+// THis solves out atomicity problem but we still have a problem we do not when the data
+// will be written to the disk (persistence problem) as when we do a write to the disk
+// the data will still be kept in mem for sometime the cpu optimizes this process
+// therefore we need some predicable behaviour  // Enterns fsync and fdatasync
+// fsync is a way to tell the cpu to flush the data to the disk
 
 
+func SaveData3(path string, data []byte) error {
+
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+	randomFileName := r.Intn(100)
+
+	tmpPath := fmt.Sprintf("%s.tmp.%d", path, randomFileName)
+
+	fp, err := os.OpenFile(tmpPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer fp.Close()
+
+	_, err = fp.Write(data)
+	if err != nil {
+		os.Remove(tmpPath)
+		log.Fatal(err)
+	}
+	
+	//All we do is call the fsync after writing the data to the file
+	// this essures any file buffers are flushed to disk
+
+	err = fp.Sync()
+	if err != nil {
+		os.Remove(tmpPath)
+		log.Fatal(err)
+	}
+
+	return os.Rename(tmpPath, path)
 }
 
 
+func main() {
 
-func main(){
-
-	s := "Hello world again";
+	s := "Hello world again"
 	dataString := []byte(s)
 
-	err := SaveData("C:\\Users\\prasa\\Downloads\\example.txt",dataString)
+	err := SaveData("C:\\Users\\prasa\\Downloads\\example.txt", dataString)
 
 	if err != nil {
 		log.Fatal(err)
